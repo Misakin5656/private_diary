572ea6a219a5722008671654b13ba60234990af1
Misakin5656 <misakin.love.c@gmail.com>
2021-09-07 19:49:27 +0000

    2021/09/07.2の分

--- diary/views.py
+++ diary/views.py
@@ -4,12 +4,16 @@ from django.views import generic
 from.forms import InquiryForm
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'diary/index.html')
+    return render(request, 'diary/index.html')
+
 
 class IndexView(generic.TemplateView):
-    template_name="index.html"
+    template_name = "index.html"
+
 
 class InquiryView(generic.FormView):
-    template_name="inquiry.html"
-    form_class=InquiryForm
+    template_name = "inquiry.html"
+    form_class = InquiryForm
