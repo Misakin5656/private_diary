4c9f99a7b45e54ad342da1f20cd928ddc03b6c8d
Misakin5656 <misakin.love.c@gmail.com>
2021-09-07 12:30:05 +0900

    2021/09/07.1

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from.forms import InquiryForm
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,7 @@ def index(request):
 
 class IndexView(generic.TemplateView):
     template_name="index.html"
+
+class InquiryView(generic.FormView):
+    template_name="inquiry.html"
+    form_class=InquiryForm
