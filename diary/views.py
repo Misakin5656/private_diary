9613895efbbebc70e763ace074908193ceae07f7
Misakin5656 <misakin.love.c@gmail.com>
2021-09-08 12:22:47 +0900

    2021/09/08.1

--- diary/views.py
+++ diary/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 from django.views import generic
 from.forms import InquiryForm
 
+import logging
+from django.urls import reverse_lazy
+
+from django.contrib import messages
+
+logger=logging.getLogger(__name__)
+
 # Create your views here.
 
 
@@ -17,3 +24,10 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url=reverse_lazy('diary:inquiry')
+
+    def from_valid(self,form):
+        form.send_email()
+        messages.success(self.request,'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
